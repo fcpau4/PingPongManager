ac78fdd915824860e3100df3804a14d554356f02
Pau <fcpau4@gmail.com>
2017-04-03 12:47:22 +0200

    Add project files.

---
+++ PingPongManager/Players.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace PingPongManager
+{
+    partial class Players
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Players));
+            this.newPlayerPn = new System.Windows.Forms.Panel();
+            this.savePb = new System.Windows.Forms.PictureBox();
+            this.restartPb = new System.Windows.Forms.PictureBox();
+            this.homePb = new System.Windows.Forms.PictureBox();
+            this.surnameLb = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.emailTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.playerImageBx = new System.Windows.Forms.PictureBox();
+            this.nameTb = new System.Windows.Forms.TextBox();
+            this.nameLb = new System.Windows.Forms.Label();
+            this.playersTc = new System.Windows.Forms.TabControl();
+            this.newPlayerTp = new System.Windows.Forms.TabPage();
+            this.updatePlayerTp = new System.Windows.Forms.TabPage();
+            this.newPlayerPn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.savePb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restartPb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homePb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerImageBx)).BeginInit();
+            this.playersTc.SuspendLayout();
+            this.newPlayerTp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // newPlayerPn
+            // 
+            this.newPlayerPn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.newPlayerPn.BackColor = System.Drawing.Color.Salmon;
+            this.newPlayerPn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.newPlayerPn.Controls.Add(this.savePb);
+            this.newPlayerPn.Controls.Add(this.restartPb);
+            this.newPlayerPn.Controls.Add(this.homePb);
+            this.newPlayerPn.Controls.Add(this.surnameLb);
+            this.newPlayerPn.Controls.Add(this.textBox1);
+            this.newPlayerPn.Controls.Add(this.emailTb);
+            this.newPlayerPn.Controls.Add(this.label1);
+            this.newPlayerPn.Controls.Add(this.playerImageBx);
+            this.newPlayerPn.Controls.Add(this.nameTb);
+            this.newPlayerPn.Controls.Add(this.nameLb);
+            this.newPlayerPn.ForeColor = System.Drawing.Color.Transparent;
+            this.newPlayerPn.Location = new System.Drawing.Point(143, 100);
+            this.newPlayerPn.Name = "newPlayerPn";
+            this.newPlayerPn.Size = new System.Drawing.Size(491, 320);
+            this.newPlayerPn.TabIndex = 0;
+            // 
+            // savePb
+            // 
+            this.savePb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("savePb.BackgroundImage")));
+            this.savePb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.savePb.Location = new System.Drawing.Point(277, 236);
+            this.savePb.Name = "savePb";
+            this.savePb.Size = new System.Drawing.Size(49, 44);
+            this.savePb.TabIndex = 9;
+            this.savePb.TabStop = false;
+            // 
+            // restartPb
+            // 
+            this.restartPb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("restartPb.BackgroundImage")));
+            this.restartPb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.restartPb.Location = new System.Drawing.Point(200, 236);
+            this.restartPb.Name = "restartPb";
+            this.restartPb.Size = new System.Drawing.Size(46, 44);
+            this.restartPb.TabIndex = 8;
+            this.restartPb.TabStop = false;
+            // 
+            // homePb
+            // 
+            this.homePb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("homePb.BackgroundImage")));
+            this.homePb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.homePb.Location = new System.Drawing.Point(121, 236);
+            this.homePb.Name = "homePb";
+            this.homePb.Size = new System.Drawing.Size(47, 44);
+            this.homePb.TabIndex = 7;
+            this.homePb.TabStop = false;
+            // 
+            // surnameLb
+            // 
+            this.surnameLb.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.surnameLb.BackColor = System.Drawing.Color.Transparent;
+            this.surnameLb.Font = new System.Drawing.Font("Gill Sans MT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.surnameLb.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.surnameLb.Location = new System.Drawing.Point(235, 97);
+            this.surnameLb.Name = "surnameLb";
+            this.surnameLb.Size = new System.Drawing.Size(199, 25);
+            this.surnameLb.TabIndex = 6;
+            this.surnameLb.Text = "Surname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // emailTb
+            // 
+            this.emailTb.Location = new System.Drawing.Point(240, 187);
+            this.emailTb.Name = "emailTb";
+            this.emailTb.Size = new System.Drawing.Size(185, 22);
+            this.emailTb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label1.Location = new System.Drawing.Point(235, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email";
+            // 
+            // playerImageBx
+            // 
+            this.playerImageBx.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.playerImageBx.BackColor = System.Drawing.Color.White;
+            this.playerImageBx.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("playerImageBx.BackgroundImage")));
+            this.playerImageBx.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.playerImageBx.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.playerImageBx.Location = new System.Drawing.Point(30, 24);
+            this.playerImageBx.Name = "playerImageBx";
+            this.playerImageBx.Size = new System.Drawing.Size(184, 185);
+            this.playerImageBx.TabIndex = 2;
+            this.playerImageBx.TabStop = false;
+            // 
+            // nameTb
+            // 
+            this.nameTb.Location = new System.Drawing.Point(240, 63);
+            this.nameTb.Name = "nameTb";
+            this.nameTb.Size = new System.Drawing.Size(185, 22);
+            this.nameTb.TabIndex = 1;
+            // 
+            // nameLb
+            // 
+            this.nameLb.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nameLb.BackColor = System.Drawing.Color.Transparent;
+            this.nameLb.Font = new System.Drawing.Font("Gill Sans MT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLb.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.nameLb.Location = new System.Drawing.Point(235, 35);
+            this.nameLb.Name = "nameLb";
+            this.nameLb.Size = new System.Drawing.Size(199, 25);
+            this.nameLb.TabIndex = 0;
+            this.nameLb.Text = "Name";
+            // 
+            // playersTc
+            // 
+            this.playersTc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.playersTc.Controls.Add(this.newPlayerTp);
+            this.playersTc.Controls.Add(this.updatePlayerTp);
+            this.playersTc.Location = new System.Drawing.Point(0, 0);
+            this.playersTc.Name = "playersTc";
+            this.playersTc.SelectedIndex = 0;
+            this.playersTc.Size = new System.Drawing.Size(829, 591);
+            this.playersTc.TabIndex = 1;
+            // 
+            // newPlayerTp
+            // 
+            this.newPlayerTp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("newPlayerTp.BackgroundImage")));
+            this.newPlayerTp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.newPlayerTp.Controls.Add(this.newPlayerPn);
+            this.newPlayerTp.Location = new System.Drawing.Point(4, 25);
+            this.newPlayerTp.Name = "newPlayerTp";
+            this.newPlayerTp.Padding = new System.Windows.Forms.Padding(3);
+            this.newPlayerTp.Size = new System.Drawing.Size(821, 562);
+            this.newPlayerTp.TabIndex = 0;
+            this.newPlayerTp.Text = "New";
+            this.newPlayerTp.UseVisualStyleBackColor = true;
+            // 
+            // updatePlayerTp
+            // 
+            this.updatePlayerTp.Location = new System.Drawing.Point(4, 25);
+            this.updatePlayerTp.Name = "updatePlayerTp";
+            this.updatePlayerTp.Padding = new System.Windows.Forms.Padding(3);
+            this.updatePlayerTp.Size = new System.Drawing.Size(821, 562);
+            this.updatePlayerTp.TabIndex = 1;
+            this.updatePlayerTp.Text = "Update";
+            this.updatePlayerTp.UseVisualStyleBackColor = true;
+            // 
+            // Players
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.playersTc);
+            this.Name = "Players";
+            this.Size = new System.Drawing.Size(832, 594);
+            this.newPlayerPn.ResumeLayout(false);
+            this.newPlayerPn.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.savePb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restartPb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homePb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerImageBx)).EndInit();
+            this.playersTc.ResumeLayout(false);
+            this.newPlayerTp.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel newPlayerPn;
+        private System.Windows.Forms.PictureBox playerImageBx;
+        private System.Windows.Forms.TextBox nameTb;
+        private System.Windows.Forms.Label nameLb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox emailTb;
+        private System.Windows.Forms.Label surnameLb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox savePb;
+        private System.Windows.Forms.PictureBox restartPb;
+        private System.Windows.Forms.PictureBox homePb;
+        private System.Windows.Forms.TabControl playersTc;
+        private System.Windows.Forms.TabPage newPlayerTp;
+        private System.Windows.Forms.TabPage updatePlayerTp;
+    }
+}
